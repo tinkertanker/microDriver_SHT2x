44a9abea1997b45a672c407b435f8acbc0da9c0b
Zhu Zhan Yan <program.nom@gmail.com>
2018-01-26 15:43:02 +0800

    Initial Commit

---
+++ extension.cpp
@@ -0,0 +1,8 @@
+#include "pxt.h"
+using namespace pxt;
+namespace sht21_driver {
+    //%
+    int extfun(int x, int y) {
+        return x + y;
+    }
+}
---
+++ extension.ts
@@ -0,0 +1,10 @@
+namespace sht21_driver {
+    /**
+     * Help goes here.
+     */
+    //% shim=sht21_driver::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
