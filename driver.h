ac5fc83a36530a8087db6fe37e5f26ab63ccaedc
Zhu Zhan Yan <program.nom@gmail.com>
2018-01-29 14:28:40 +0800

    Change return type of read functions to integer.

--- driver.h
+++ driver.h
@@ -6,10 +6,11 @@
 #include <stddef.h>
 #include "pxt.h"
 
-#define DEBUG 1
+//#define DEBUG 1
 
 /* DEBUG PRINT */
 #ifdef DEBUG
+#undef printf
 #define dprint(msg) uBit.serial.printf("DEBUG: %d: %s: %s\r\n",__LINE__,__func__,msg)
 #define dprintf(...) uBit.serial.printf("DEBUG: " __VA_ARGS__)
 #else
@@ -19,13 +20,13 @@
 #endif /* DEBUG PRINT */
 
 /* Address for talking to i2c */
-#define SHT2X_I2C_ADDR 0x40
+#define SHT2X_I2C_ADDR 0x80
 
 #define SHT2X_PANIC_CODE 80
 
 /* Macros for converting data from the sensor to human readable measurements */
-#define SHT_CONV_TEMP(val) (-46.85 + 175.72 / 65536.0 * (double) val)
-#define SHT_CONV_HUMID(val) (-6.0 + 125.0 / 65536.0 * (double) val)
+#define SHT_CONV_TEMP(val) (-46.85 + (175.72 / 65536.0 * (double) val))
+#define SHT_CONV_HUMID(val) (-6.0 + (125.0 / 65536.0 * (double) val))
 
 namespace SHT2xDriver 
 {
@@ -36,16 +37,18 @@ namespace SHT2xDriver
     typedef enum sht_command_t
     {
         sht_command_temperture_hold = 0xE3,
-        sht_command_temperture = 0xE5,
-        sht_command_humidity_hold = 0xF3,
+        sht_command_humidity_hold  = 0xE5,
+        sht_command_temperture = 0xF3,
         sht_command_humidity = 0xF5
     }SHTCommand;
     
     /* Read humidity level from the sensor in %Relative Humidity*/
-    double read_humidity();
+    //%
+    int read_humidity();
 
-    /* Read tempreture from the sensor in degree's celcius*/
-    double read_tempreture();
+    /* Read temperture from the sensor in degree's celcius*/
+    //%
+    int read_temperture();
 }
 
 #endif /* ifndef SHT2x_DRIVER_H */
--- driver.ts
+++ driver.ts
@@ -1,33 +1,33 @@
+/**
+ * The SHT2x Driver provides access to the functionality of the SHT20, SHT21 
+ * and SHT2x sensors
+*/
+//% color=#4c6ef5 weight=25 icon="\uf043" block="SHT2x Sensor"
 namespace SHT2xDriver {
     /**
-     * Read %Relative Humidity from the sensor.
-     * Relative Humidity is the amount of water vapor in the air, expressed as 
-     * a percentage of the maximum amount that the air could hold at the given 
-     * temperature.
-     *
+     * Read Relative Humidity from the SHT2x Sensor.
      * @return number describing the relative humidity in percentage % relative
      * humidity
-     */
+    */
     //% shim=SHT2xDriver::read_humidity
     //% blockId="SHT2xDriver_read_humidity"
     //% block="read humidty"
-    export function read_humidity(): number{
+    export function read_humidity() {
         // Dummy implementation for the simulator.
-        console.log("Simulate: Read Humidity from SHT2x sensor");
+        console.log("Simulate: Read Humidity from SHT2x sensor: 50");
         return 50.0;
     }
 
     /**
-     * Read Temperature in degrees celcius from the sensor.
-     *
-     * @return number describing the ambient tempreture in degrees celcius
-     */
+     * Read Temperature in degrees celcius from the SHT2x sensor.
+     * @return number describing the ambient temperture in degrees celcius
+    */
     //% shim=SHT2xDriver::read_temperture
     //% blockId="SHT2xDriver_read_temperature"
-    //% block="read tempreture"
-    export function read_temperture(): number{
+    //% block="read temperture"
+    export function read_temperture(){
         // Dummy implementation for the simulator.
-        console.log("Simulate: Read Temperature from SHT2x sensor");
+        console.log("Simulate: Read Temperature from SHT2x sensor: 50");
         return 30.0;
     }
 }
