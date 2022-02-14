d2ec3d9430467153b435eb5ca022f824a311cd11
Mireille T <69097387+Mireille-T@users.noreply.github.com>
2022-02-14 16:49:23 +0800

    Fix for v2 compatibility

--- driver_sht2x.ts
+++ driver_sht2x.ts
@@ -2,6 +2,9 @@
  * The SHT2x Driver provides access to the functionality of the SHT20, SHT21 
  * and SHT2x sensors
 */
+
+let sht_i2c_address = 0x40;
+
 //% color=#4c6ef5 weight=25 icon="\uf043" block="SHT2x Sensor"
 namespace SHT2xDriver {
     /**
@@ -9,41 +12,43 @@ namespace SHT2xDriver {
      * Returns a number describing the relative humidity in percentage % relative
      * humidity
     */
-    //% shim=SHT2xDriver::read_humidity
     //% blockId="SHT2xDriver_read_humidity"
     //% block="read humidty"
-    export function read_humidity(): number{
-        // Dummy implementation for the simulator.
-        console.log("Simulate: Read Humidity from SHT2x sensor: 50");
-        return 50.0;
+    export function read_humidity(): number {
+        pins.i2cWriteNumber(sht_i2c_address, 0xF3, NumberFormat.UInt8LE, false);
+        basic.pause(100);
+        let buff = pins.i2cReadBuffer(sht_i2c_address, 3);
+        let result = buff[0] << 8;
+        result |= buff[1];
+        result = (((15625 * result) >> 13) - 6000) / 1000;
+        return result;
     }
 
     /**
      * Read Temperature in degrees celcius from the SHT2x sensor.
      * Returns a number describing the ambient temperature in degrees celcius
     */
-    //% shim=SHT2xDriver::read_temperature
     //% blockId="SHT2xDriver_read_temperature"
     //% block="read temperature"
-    export function read_temperature(): number{
-        // Dummy implementation for the simulator.
-        console.log("Simulate: Read Temperature from SHT2x sensor: 50");
-        return 30.0;
+    export function read_temperature(): number {
+        pins.i2cWriteNumber(sht_i2c_address, 0xF3, NumberFormat.UInt8LE, false);
+        basic.pause(100);
+        let buff = pins.i2cReadBuffer(sht_i2c_address, 3);
+        let result = buff[0] << 8;
+        result |= buff[1];
+        result = (((21965 * result) >> 13) - 46850) / 1000;
+        return result;
     }
 
     /** 
      * Change the I2c address used to interact with the SHT2x to the given
      * address. The I2c address should be within 0-127.
     */
-    //% shim=SHT2xDriver::set_i2c_address
     //% blockId="SHT2xDriver_set_i2c_address"
     //% block="Change i2c address to %address"
     //% advanced=true
-    export function set_i2c_address(address:number)
-    {
-        // Dummy implementation for the simulator.
-        if(address < 0 || address > 128) 
-        { console.log("Simulate: Invaild i2c Address: "  + address); }
-        else { console.log("Simulate: Set i2c address to " + address); }
+    export function set_i2c_address(address: number) {
+        if (address < 0 || address > 128) basic.showString("Invalid I2C address");
+        else sht_i2c_address = address;
     }
 }
